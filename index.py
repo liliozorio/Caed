5b405881aa4bd3f1dbfc2dda8e978bb2bbbd3dea
Livia <57274418+liliozorio@users.noreply.github.com>
2022-03-06 13:32:25 -0300

    Update index.py

--- index.py
+++ index.py
@@ -4,19 +4,20 @@ import pandas as pd
 import random
 import seaborn as sns
 
-dataframe = pd.read_csv('/Dataset/training.csv')
+dataframe = pd.read_csv('/dataset/dataset_caed/training.csv')
 
 grau_SVC = []
 grau_NNK = []
 frases_modificadas = []
  
 for i in range(0,100):
-  randomico = ramdom.randint(0,len(dataframe)
+  randomico = random.randint(0,len(dataframe)-1)
   frase = dataframe['text'][randomico]
   modificada = muda_sinonimo(frase)
   modificada = muda_genero(modificada)
-  classificacao_SVC = predicao(modificada)[0]
-  classificacao_NNK = predicao(modificada)[1]
+  classificacao_geral = predicao(modificada)
+  classificacao_SVC = classificacao_geral[0]
+  classificacao_NNK = classificacao_geral[1]
   frases_modificadas.append(modificada)
   grau_NNK.append(dataframe['level'][randomico] - classificacao_SVC)
   grau_SVC.append(dataframe['level'][randomico] - classificacao_NNK)
