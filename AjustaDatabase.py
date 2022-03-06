361c2c7e970f00b825c92a89130ea45931aa10f4
Livia <57274418+liliozorio@users.noreply.github.com>
2022-03-06 19:58:32 -0300

    Update AjustaDatabase.py

--- AjustaDatabase.py
+++ AjustaDatabase.py
@@ -4,68 +4,116 @@ Import sys
 
 local = str(sys.argv[1])
 #Cria bases
-lista1 = []
-lista2 = []
-lista3 = []
-lista4 = []
-
-for i in range(1,827):
-  if i < 298:
-    lista1.append(i)
-    lista2.append(i)
-    lista3.append(i)
-    lista4.append(i)
-  elif i < 326:
-    lista2.append(i)
-    lista3.append(i)
-    lista4.append(i)
-  elif i < 629:
-    lista3.append(i)
-    lista4.append(i)
-  else:
-    lista4.append(i)
-
-random.shuffle(lista1)
-random.shuffle(lista2)
-random.shuffle(lista3)
-random.shuffle(lista4)
-
-traning = pd.DataFrame(columns=['text', 'level'])
-test = pd.DataFrame(columns=['text', 'level'])
-
-for i in range(0,20):
-  frase1 = open("/Dataset/dataset_caed/1_Ensino_Fundamental_I/" + str(lista1[i]) + ".txt")
-  frase1 = frase1.read()
-  frases = frase1.split('.')
-  for frase in frases:
-    frase = frase.replace('\n', '')
-    if len(frase) > 2:
-      newrow1 = {'text':frase,'level':'1'}
+if local == 'frase':
+  lista1 = []
+  lista2 = []
+  lista3 = []
+  lista4 = []
+  
+  for i in range(1,827):
+    if i < 298:
+      lista1.append(i)
+      lista2.append(i)
+      lista3.append(i)
+      lista4.append(i)
+    elif i < 326:
+      lista2.append(i)
+      lista3.append(i)
+      lista4.append(i)
+    elif i < 629:
+      lista3.append(i)
+      lista4.append(i)
+    else:
+      lista4.append(i)
+  
+  random.shuffle(lista1)
+  random.shuffle(lista2)
+  random.shuffle(lista3)
+  random.shuffle(lista4)
+  
+  traning = pd.DataFrame(columns=['text', 'level'])
+  test = pd.DataFrame(columns=['text', 'level'])
+  
+  for i in range(0,20):
+    frase1 = open("/Dataset/dataset_caed/1_Ensino_Fundamental_I/" + str(lista1[i]) + ".txt")
+    frase1 = frase1.read()
+    frases = frase1.split('.')
+    for frase in frases:
+      frase = frase.replace('\n', '')
+      if len(frase) > 2:
+        newrow1 = {'text':frase,'level':'1'}
+        traning = traning.append(newrow1, ignore_index=True) 
+    frase2 = open("/Dataset/dataset_caed/2_Ensino_Fundamental_II/" + str(lista2[i]) + ".txt")
+    frase2 = frase2.read()
+    frases = frase2.split('.')
+    for frase in frases:
+      frase = frase.replace('\n', '')
+      if len(frase) > 2:
+        newrow2 = {'text':frase,'level':'2'}
+        traning = traning.append(newrow2, ignore_index=True) 
+    frase3 = open("/Dataset/dataset_caed/3_Ensino_Medio/" + str(lista3[i]) + ".txt")
+    frase3 = frase3.read()
+    frases = frase3.split('.')
+    for frase in frases:
+      frase = frase.replace('\n', '')
+      if len(frase) > 2:
+        newrow3 = {'text':frase,'level':'3'}
+        traning = traning.append(newrow3, ignore_index=True) 
+    frase4 = open("/Dataset/dataset_caed/4_Ensino_Superior/" + str(lista4[i]) + ".txt")
+    frase4 = frase4.read()
+    frases = frase4.split('.')
+    for frase in frases:
+      frase = frase.replace('\n', '')
+      if len(frase) > 2:
+        newrow4 = {'text':frase,'level':'4'}
+        traning = traning.append(newrow4, ignore_index=True) 
+  elif local == 'texto':
+    lista1 = []
+    lista2 = []
+    lista3 = []
+    lista4 = []
+    
+    for i in range(1,827):
+      if i < 298:
+        lista1.append(i)
+        lista2.append(i)
+        lista3.append(i)
+        lista4.append(i)
+      elif i < 326:
+        lista2.append(i)
+        lista3.append(i)
+        lista4.append(i)
+      elif i < 629:
+        lista3.append(i)
+        lista4.append(i)
+      else:
+        lista4.append(i)
+    
+    random.shuffle(lista1)
+    random.shuffle(lista2)
+    random.shuffle(lista3)
+    random.shuffle(lista4)
+    
+    traning = pd.DataFrame(columns=['text', 'level'])
+    test = pd.DataFrame(columns=['text', 'level'])
+    
+    for i in range(0,20):
+      frase1 = open("/Dataset/dataset_caed/1_Ensino_Fundamental_I/" + str(lista1[i]) + ".txt")
+      frase1 = frase1.read()
+      newrow1 = {'text':frase1,'level':'1'}
       traning = traning.append(newrow1, ignore_index=True) 
-  frase2 = open("/Dataset/dataset_caed/2_Ensino_Fundamental_II/" + str(lista2[i]) + ".txt")
-  frase2 = frase2.read()
-  frases = frase2.split('.')
-  for frase in frases:
-    frase = frase.replace('\n', '')
-    if len(frase) > 2:
-      newrow2 = {'text':frase,'level':'2'}
+      frase2 = open("/Dataset/dataset_caed/2_Ensino_Fundamental_II/" + str(lista2[i]) + ".txt")
+      frase2 = frase2.read()
+      newrow2 = {'text':frase2,'level':'2'}
       traning = traning.append(newrow2, ignore_index=True) 
-  frase3 = open("/Dataset/dataset_caed/3_Ensino_Medio/" + str(lista3[i]) + ".txt")
-  frase3 = frase3.read()
-  frases = frase3.split('.')
-  for frase in frases:
-    frase = frase.replace('\n', '')
-    if len(frase) > 2:
-      newrow3 = {'text':frase,'level':'3'}
+      frase3 = open("/Dataset/dataset_caed/3_Ensino_Medio/" + str(lista3[i]) + ".txt")
+      frase3 = frase3.read()
+      newrow3 = {'text':frase3,'level':'3'}
       traning = traning.append(newrow3, ignore_index=True) 
-  frase4 = open("/Dataset/dataset_caed/4_Ensino_Superior/" + str(lista4[i]) + ".txt")
-  frase4 = frase4.read()
-  frases = frase4.split('.')
-  for frase in frases:
-    frase = frase.replace('\n', '')
-    if len(frase) > 2:
-      newrow4 = {'text':frase,'level':'4'}
-      traning = traning.append(newrow4, ignore_index=True) 
+      frase4 = open("/Dataset/dataset_caed/4_Ensino_Superior/" + str(lista4[i]) + ".txt")
+      frase4 = frase4.read()
+      newrow4 = {'text':frase4,'level':'4'}
+      traning = traning.append(newrow4, ignore_index=True)
 traning.to_csv('/Dataset/training_ ' + local +'.csv', index = False)
 
 #Ajusta Dataset
